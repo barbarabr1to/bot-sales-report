e0163aeb9acd5b43b8a2a32ed4c3eb6461892309
Barbara Brito <barbarabritosz@hotmail.com>
2021-08-14 17:11:35 -0300

    Projeto iniciado

---
+++ sales_report.py
@@ -0,0 +1,6 @@
+# Passo 1: Abrir navegador
+# Passo 2: Acessar link - https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing
+# Passo 3: Entrar na pasta "exportar"
+# Passo 4: Baixa arquivo - Vendas - Dez
+# Passo 5: Calcular a quantiade e o faturamento dos produtos 
+# Passo 6: Enviar email para diretoria
