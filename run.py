0c50dd56ce55c37b6db2a9989c9403ced1e91012
Barbara Brito <barbarabritosz@hotmail.com>
2021-08-15 23:03:40 -0300

    Funções open_browser e access_link

---
+++ run.py
@@ -0,0 +1,4 @@
+import sales_report
+
+sales_report.open_browser()
+sales_report.access_link()
--- sales_report.py
+++ sales_report.py
@@ -1,6 +1,28 @@
+import pyautogui as auto
+import pyperclip
+import time 
+
+
 # Passo 1: Abrir navegador
 # Passo 2: Acessar link - https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing
 # Passo 3: Entrar na pasta "exportar"
 # Passo 4: Baixa arquivo - Vendas - Dez
 # Passo 5: Calcular a quantiade e o faturamento dos produtos 
 # Passo 6: Enviar email para diretoria
+
+
+auto.PAUSE = 1
+
+# MELHORIA: deixar resiliente a qualquer SO e browser
+def open_browser():
+    auto.hotkey('WIN', 'r')
+    auto.write("C:\Program Files\Google\Chrome\Application\chrome.exe", interval=0.05)
+    return auto.press('ENTER')
+
+# MELHORIA: deixar resiliente caso não tenha conta google logada
+def access_link():
+    time.sleep(2)
+    pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing")
+    auto.hotkey('CTRL', 'v')
+    return auto.hotkey('ENTER')
+
