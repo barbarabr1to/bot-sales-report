0c811e8f2489042e99952f364dac5d5408c61568
Barbara Brito <barbarabritosz@hotmail.com>
2021-08-15 23:51:30 -0300

    Função calculate

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import sales_report
 
-sales_report.open_browser()
-sales_report.access_link()
-sales_report.enter_folder()
-sales_report.download_file()
+# sales_report.open_browser()
+# sales_report.access_link()
+# sales_report.enter_folder()
+# sales_report.download_file()
+sales_report.calculate()
--- sales_report.py
+++ sales_report.py
@@ -1,4 +1,5 @@
 import pyautogui as auto
+import pandas    as pd
 import pyperclip
 import time 
 
@@ -16,13 +17,13 @@ auto.PAUSE = 1
 # MELHORIA: deixar resiliente a qualquer SO e browser
 def open_browser():
     auto.hotkey('WIN', 'r')
-    auto.write("C:\Program Files\Google\Chrome\Application\chrome.exe", interval=0.05)
+    auto.write('C:\Program Files\Google\Chrome\Application\chrome.exe', interval=0.05)
     return auto.press('ENTER')
 
 # MELHORIA: deixar resiliente caso não tenha conta google logada
 def access_link():
     time.sleep(2)
-    pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing")
+    pyperclip.copy('https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing')
     auto.hotkey('CTRL', 'v')
     return auto.hotkey('ENTER')
 
@@ -35,3 +36,12 @@ def download_file():
     time.sleep(2)
     auto.click(x=550, y=540, button='right')
     return auto.click(x=720, y=950)
+
+# MELHORIA: identificar caminho de forma dinânmica
+def calculate():
+    spreadsheet = pd.read_excel(r'C:\Users\Bárbara\Downloads\Vendas - Dez (1).xlsx')
+    quantity = spreadsheet['Quantidade'].sum()
+    revenues = spreadsheet['Valor Final'].sum()
+    print('\nQuantidade de produtos:', quantity)
+    print(f'Faturamento: R$ {revenues},00\n')
+    return quantity, revenues
